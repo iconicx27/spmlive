56caa981d9144d8f75014d3b3a508c7674a77d68
tejas <sonartejas00@gmail.com>
2023-03-02 12:43:53 +0530

    2/3/2023

--- apps/web/pages/homepage.js
+++ apps/web/pages/homepage.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Navbar from "../components/navbar.js"
 // import Carousel from '../components/carousel.js'
 import Footer from '../components/footer.js'
+import ProductList from '../components/productList.js'
 const homepage = (props) => {
     return (
         <>
         <Navbar/>
-        {/* <Carousel/> */}
+
+        {/* <!-- Title --> */}
+        <div class="pt-28 bg-gray-100">
+        <h1 class="text-center text-2xl font-bold text-gray-800">All Products</h1>
+        </div>
+
+
+        <ProductList/>
         <Footer/>
         </>
     )
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,21 @@
 import Navbar from '../components/navbar.js'
-
+import React from 'react'
+// import Carousel from '../components/carousel.js'
+import Footer from '../components/footer.js'
+import ProductList from '../components/productList.js'
 export default function Home() {
   return (
-    <div>hey!</div>
+    <>
+        <Navbar/>
+
+        {/* <!-- Title --> */}
+        <div className="pt-28 bg-gray-100">
+        <h1 className="text-center text-2xl font-bold text-gray-800">All Products</h1>
+        </div>
+
+
+        <ProductList/>
+        <Footer/>
+        </>
   )
 }
